02a19d17f1bcee2062e80bb11c1868d3f5eda61b
caolei <2804406646@qq.com>
2022-06-14 21:29:14 +0800

    faeat:init

---
+++ .umirc.ts
@@ -0,0 +1,15 @@
+// import router from './router'
+export default {
+  npmClient: "pnpm",
+  apiRoute: {
+    platform: "vercel",
+  },
+  routes: [
+    { exact: true, path: "/", component: "index" },
+    { exact: true, path: "/posts/create", component: "posts/create" },
+    { exact: true, path: "/login", component: "login" },
+    { exact: true, path: "/posts/:postId", component: "posts/post" },
+  ],
+  plugins: [require.resolve("@umijs/plugins/dist/tailwindcss")],
+  tailwindcss: {},
+};
