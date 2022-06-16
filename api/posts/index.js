638c4b7693b55bbdb4f5be83d9b7c28c977c5a74
caolei <2804406646@qq.com>
2022-06-16 21:56:54 +0800

    feat:init

--- api/posts/index.js
+++ api/posts/index.js
@@ -253,7 +253,39 @@ async function posts_default(req, res) {
 
 // src/.umi/api/posts/index.ts
 var import_apiRoute = __toESM(require_apiRoute());
-var apiRoutes = [{ "path": "posts/[postId]", "id": "posts/[postId]", "file": "posts/[postId].ts", "absPath": "/posts/[postId]", "__content": 'import { UmiApiRequest, UmiApiResponse } from "umi";\n \nexport default async function (req: UmiApiRequest, res: UmiApiResponse) {\n  res.status(400).json({ error: "This API is not implemented yet." })\n}' }, { "path": "posts", "id": "posts/index", "file": "posts/index.ts", "absPath": "/posts", "__content": 'import { UmiApiRequest, UmiApiResponse } from "umi";\n \nexport default async function (req: UmiApiRequest, res: UmiApiResponse) {\n  res.status(400).json({ error: "This API is not implemented yet." })\n}' }, { "path": "register", "id": "register", "file": "register.ts", "absPath": "/register", "__content": 'import { UmiApiRequest, UmiApiResponse } from "umi";\n \nexport default async function (req: UmiApiRequest, res: UmiApiResponse) {\n  res.status(400).json({ error: "This API is not implemented yet." })\n}' }, { "path": "login", "id": "login", "file": "login.ts", "absPath": "/login", "__content": 'import { UmiApiRequest, UmiApiResponse } from "umi";\n \nexport default async function (req: UmiApiRequest, res: UmiApiResponse) {\n  res.status(400).json({ error: "This API is not implemented yet." })\n}' }];
+var apiRoutes = [{ "path": "posts/[postId]", "id": "posts/[postId]", "file": "posts/[postId].ts", "absPath": "/posts/[postId]", "__content": 'import { UmiApiRequest, UmiApiResponse } from "umi";\n \nexport default async function (req: UmiApiRequest, res: UmiApiResponse) {\n  res.status(400).json({ error: "This API is not implemented yet." })\n}' }, { "path": "posts", "id": "posts/index", "file": "posts/index.ts", "absPath": "/posts", "__content": 'import { UmiApiRequest, UmiApiResponse } from "umi";\n \nexport default async function (req: UmiApiRequest, res: UmiApiResponse) {\n  res.status(400).json({ error: "This API is not implemented yet." })\n}' }, { "path": "register", "id": "register", "file": "register.ts", "absPath": "/register", "__content": `import { UmiApiRequest, UmiApiResponse } from "umi";
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcryptjs'
+// import { signToken } from "@/utils/jwt";
+
+export default async function (req: UmiApiRequest, res: UmiApiResponse) {
+  switch (req.method) {
+    case 'POST':
+      try {
+        const prisma = new PrismaClient();
+        const user = await prisma.user.create({
+          data: {
+            email: req.body.email,
+            passwordHash: bcrypt.hashSync(req.body.password, 8),
+            name: req.body.name,
+            avatarUrl: req.body.avatarUrl
+          }
+        });
+        res.status(201)
+          // .setCookie('token', await signToken(user.id))
+          .json({ ...user, passwordHash: undefined })
+        await prisma.$disconnect()
+      } catch (e: any) {
+        res.status(500).json({
+          result: false,
+          message: typeof e.code === 'string' ? 'https://www.prisma.io/docs/reference/api-reference/error-reference#' + e.code.toLowerCase() : e
+        })
+      }
+      break;
+    default:
+      res.status(405).json({ error: 'Method not allowed' })
+  }
+}` }, { "path": "login", "id": "login", "file": "login.ts", "absPath": "/login", "__content": 'import { UmiApiRequest, UmiApiResponse } from "umi";\n \nexport default async function (req: UmiApiRequest, res: UmiApiResponse) {\n  res.status(400).json({ error: "This API is not implemented yet." })\n}' }];
 var posts_default2 = async (req, res) => {
   const umiReq = new import_apiRoute.UmiApiRequest(req, apiRoutes);
   await umiReq.readBody();
